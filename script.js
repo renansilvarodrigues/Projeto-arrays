3ba7174c528baeb848c64b4f5a14ca27d45f608c
renansilvarodrigues <renansilva6769@gmail.com>
2025-01-17 23:49:57 -0300

    commit2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const list = document.querySelector('ul')
-const product = { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' }
+const buttonShowAll = document.querySelector('.show-all')
+let myLi = ''
 
-list.innerHTML=
-`<li>
-            <img src="${product.src}" alt="">
-            <p>${product.name}</p>
-            <p class="${product.price}">R$30,00</p>
-        </li>`
+function showAll() {
+
+    menuOptions.forEach(product => {
+        myLi += `
+         <li>
+             <img src="${product.src}" alt="">
+             <p>${product.name}</p>
+             <p class="${product.price}">R$30,00</p>
+         </li>
+         `
+    })
+
+    list.innerHTML = myLi
+
+}
+
+buttonShowAll.addEventListener('click', showAll)
